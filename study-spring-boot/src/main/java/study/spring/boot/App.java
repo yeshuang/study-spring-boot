022b20f8b191c586a1daa1f547b6dcd32a4857ee
yeshuang <ye_shuang@139.com>
2017-12-05 10:28:31 +0800

    Initial creation of a project

---
+++ study-spring-boot/src/main/java/study/spring/boot/App.java
@@ -0,0 +1,13 @@
+package study.spring.boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
